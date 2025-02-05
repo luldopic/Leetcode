d00a24bcf40a3bf81fb208f53021caeede589e62
luldopic <27344798+luldopic@users.noreply.github.com>
2025-02-05 11:54:12 +0100

    Solution 1. Time Complexity O(m*n). Space Complexity O(m*n)

--- 62_UniquePaths.py
+++ 62_UniquePaths.py
@@ -1,7 +1,29 @@
 from solution_check import ProblemSet
 
 def UniquePath(m,n):
-    pass
+    """
+    :type m: int
+    :type n: int
+    :rtype: int
+    """
+    grid = [[0 for _ in range(n)] for _ in range(m)]
+    
+    grid[0][0] = 1
+    for m_index in range(m):
+        for n_index in range(n):
+            if m_index == 0 and n_index == 0:
+                continue
+            if m_index == 0:
+                up = 0
+            else:
+                up = grid[m_index-1][n_index]
+            if n_index == 0:
+                left = 0
+            else:
+                left = grid[m_index][n_index-1]
+            grid[m_index][n_index] = up + left
+    
+    return grid[m-1][n-1]
 
 if __name__ == "__main__":
     testcases = {
